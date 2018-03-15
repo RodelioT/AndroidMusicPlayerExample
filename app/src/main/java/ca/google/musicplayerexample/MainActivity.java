de2e8049f59fd20e899fea1b3d49dcdfb872ae50
Rodelio (RJ) Tagle <rodelio.tagle@hotmail.com>
2018-03-15 13:43:21 -0500

    Collected all song data
    Finished up to "3. Display the Songs"

--- app/src/main/java/ca/google/musicplayerexample/MainActivity.java
+++ app/src/main/java/ca/google/musicplayerexample/MainActivity.java
@@ -1,13 +1,77 @@
 package ca.google.musicplayerexample;
 
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ArrayList<Song> songList;
+    private ListView songView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        songView = findViewById(R.id.song_list);
+
+        // Instantiate the song ArrayList
+        songList = new ArrayList<Song>();
+
+        // Get all songs and their data
+        getSongList();
+    }
+
+    // Method to retrieve song metadata
+    public void getSongList() {
+        //retrieve song info
+        ContentResolver musicResolver = getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+
+        if(musicCursor!=null && musicCursor.moveToFirst()){
+            //get columns
+            int titleColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+            //add songs to list
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+                songList.add(new Song(thisId, thisTitle, thisArtist));
+            }
+            while (musicCursor.moveToNext());
+        }
+    }
+
+    public class Song {
+        private long id;
+        private String title;
+        private String artist;
+        // You can add more song metadata here if you wish
+
+        // Constructor method
+        public Song(long songID, String songTitle, String songArtist) {
+            id=songID;
+            title=songTitle;
+            artist=songArtist;
+        }
+
+        // GET methods for the parameters
+        public long getID(){return id;}
+        public String getTitle(){return title;}
+        public String getArtist(){return artist;}
     }
 }
